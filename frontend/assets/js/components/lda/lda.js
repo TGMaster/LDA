4ce84d6f26d26a74d642b143ee2b721cb963ee1f
TGMaster <tien.grimes@gmail.com>
2019-07-10 08:17:17 +0700

    - done frontend!!!

--- frontend/assets/js/components/lda/lda.js
+++ frontend/assets/js/components/lda/lda.js
@@ -1,13 +1,79 @@
-'use strict';
-
 angular.module('springboot', [])
 
 	.controller("LDAController", [
 		'$scope',
 		'Util',
 		'API',
-		'$state',
-		function ($scope, Util, API, $state) {
+		function ($scope, Util, API) {
+
+			$scope.loadModel = function () {
+
+				var json = {
+					"name": "data",
+					"children": [
+						{
+							"name": "topics",
+							"children": angular.fromJson($scope.result)
+						}
+					]
+				};
+
+				var r = 1500,
+					format = d3.format(",d"),
+					fill = d3.scale.category20c();
+
+				var bubble = d3.layout.pack()
+					.sort(null)
+					.size([r, r])
+					.padding(1.5);
+
+				var vis = d3.select(".model").append("svg")
+					.attr("width", r)
+					.attr("height", r)
+					.attr("class", "bubble");
+
+
+				var node = vis.selectAll("g.node")
+					.data(bubble.nodes(classes(json))
+						.filter(function (d) { return !d.children; }))
+					.enter().append("g")
+					.attr("class", "node")
+					.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
+				color = d3.scale.category20();
+
+				node.append("title")
+					.text(function (d) { return d.className + ": " + format(d.value); });
+
+				node.append("circle")
+					.attr("r", function (d) { return d.r; })
+					.style("fill", function (d) { return color(d.topicName); });
+
+				var text = node.append("text")
+					.attr("text-anchor", "middle")
+					.attr("dy", ".3em")
+					.text(function (d) { return d.className.substring(0, d.r / 3) });
+
+				text.append("tspan")
+					.attr("dy", "1.2em")
+					.attr("x", 0)
+					.text(function (d) { return Math.ceil(d.value * 10000) / 10000; });
+
+				// Returns a flattened hierarchy containing all leaf nodes under the root.
+				function classes(root) {
+					var classes = [];
+
+					function recurse(term, node) {
+						if (node.children) node.children.forEach(function (child) { recurse(node.term, child); });
+						else classes.push({ topicName: node.topicId, className: node.term, value: node.probability });
+					}
+
+					recurse(null, root);
+					return { children: classes };
+				};
+
+
+			};
+
 
 			$scope.submitting = false;
 			$scope.trainModel = function () {
@@ -23,7 +89,7 @@ angular.module('springboot', [])
 					if (status === 200) {
 						Util.showSuccessToast("Train LDA Model Successfully!");
 						$scope.submitting = false;
-						$scope.result = "[" + response.data +"]";
+						$scope.result = "[" + response.data + "]";
 						$scope.loadModel();
 					} else {
 						var err = _.find(APIStatus, { status: status });
@@ -34,72 +100,6 @@ angular.module('springboot', [])
 					$scope.submitting = false;
 				});
 			};
-
-			$scope.loadModel = function () {
-
-				var json = {
-					"name": "data",
-					"children": [
-						{
-							"name": "topics",
-							"children": angular.fromJson($scope.result)
-						}
-					]
-				};
-	
-				var r = 1500,
-					format = d3.format(",d"),
-					fill = d3.scale.category20c();
-	
-				var bubble = d3.layout.pack()
-					.sort(null)
-					.size([r, r])
-					.padding(1.5);
-	
-				var vis = d3.select(".model").append("svg")
-					.attr("width", r)
-					.attr("height", r)
-					.attr("class", "bubble");
-	
-	
-				var node = vis.selectAll("g.node")
-					.data(bubble.nodes(classes(json))
-						.filter(function (d) { return !d.children; }))
-					.enter().append("g")
-					.attr("class", "node")
-					.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
-				color = d3.scale.category20();
-	
-				node.append("title")
-					.text(function (d) { return d.className + ": " + format(d.value); });
-	
-				node.append("circle")
-					.attr("r", function (d) { return d.r; })
-					.style("fill", function (d) { return color(d.topicName); });
-	
-				var text = node.append("text")
-					.attr("text-anchor", "middle")
-					.attr("dy", ".3em")
-					.text(function (d) { return d.className.substring(0, d.r / 3) });
-	
-				text.append("tspan")
-					.attr("dy", "1.2em")
-					.attr("x", 0)
-					.text(function (d) { return Math.ceil(d.value * 10000) / 10000; });
-			};
 		}
 		//
 	]);
-
-// Returns a flattened hierarchy containing all leaf nodes under the root.
-function classes(root) {
-	var classes = [];
-
-	function recurse(term, node) {
-		if (node.children) node.children.forEach(function (child) { recurse(node.term, child); });
-		else classes.push({ topicName: node.topicId, className: node.term, value: node.probability });
-	}
-
-	recurse(null, root);
-	return { children: classes };
-};
